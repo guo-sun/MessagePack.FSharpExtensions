54f32c18d27a5c68ed28fa639cc5765a0ebd6b1f
Steven <steven.s32@gmail.com>
2020-02-20 23:54:18 -0500

    Remove comments, tests only updated to v2 api

--- tests/MessagePack.FSharpExtensions.Tests/Helper.fs
+++ tests/MessagePack.FSharpExtensions.Tests/Helper.fs
@@ -6,28 +6,17 @@ open MessagePack
 open MessagePack.Resolvers
 open MessagePack.FSharp
 
-type WithFSharpDefaultResolverOut(out: string -> unit) =
-    interface IFormatterResolver with
-        member __.GetFormatter<'T>() =
-            out "fsharp get formatter"
-            match FSharpResolver.Instance.GetFormatter<'T>() with
-            | null ->
-                out "Didn't find fsharp resolver"
-                StandardResolver.Instance.GetFormatter<'T>()
-            | x ->
-                out "Found fsharp resolver"
-                x
+type WithFSharpDefaultResolver() =
+  interface IFormatterResolver with
+    member __.GetFormatter<'T>() =
+      match FSharpResolver.Instance.GetFormatter<'T>() with
+      | null -> StandardResolver.Instance.GetFormatter<'T>()
+      | x -> x
 
-let WithFSharpDefaultResolver() = WithFSharpDefaultResolverOut(ignore)
+let convert<'T> (value: 'T) =
+  let resolver = WithFSharpDefaultResolver() :> IFormatterResolver
+  let options = MessagePackSerializerOptions.Standard.WithResolver(resolver)
+  
+  let bin = ReadOnlyMemory(MessagePackSerializer.Serialize(value, options))
 
-let convertOut<'T> out (value: 'T) =
-    let resolver = WithFSharpDefaultResolverOut(out) :> IFormatterResolver
-    let options = MessagePackSerializerOptions.Standard.WithResolver(resolver)
-    out "Built options"
-
-    let serialized = MessagePackSerializer.Serialize(value, options)
-    let readOnlyMemory = ReadOnlyMemory(serialized)
-    MessagePackSerializer.Deserialize<'T>(readOnlyMemory, options)
-
-let convert (value: 'T) =
-        convertOut ignore value
+  MessagePackSerializer.Deserialize<'T>(bin, options)
