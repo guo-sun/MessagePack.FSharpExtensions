47c46bfc2ecde071f50045a7ef944bc891b5fa6b
Steven <steven.s32@gmail.com>
2020-03-02 15:04:43 -0500

    Target net48

--- tests/MessagePack.FSharpExtensions.Tests/DUTest.fs
+++ tests/MessagePack.FSharpExtensions.Tests/DUTest.fs
@@ -10,29 +10,7 @@ type SimpleUnion =
   | B of int
   | C of int64 * float32
 
-[<Fact>]
-let simple () =
-
-  let input = A
-  let actual = convert input
-  Assert.Equal(input, actual)
-
-  let input = B 100
-  let actual = convert input
-  Assert.Equal(input, actual)
-
-  let input = C(99999999L, -123.43f)
-  let actual = convert input
-  Assert.Equal(input, actual)
-
 type StringKeyUnion = | D of Prop : int
-
-[<Fact>]
-let ``string key`` () =
-
-  let input = D 1
-  let actual = convert input
-  Assert.Equal(input, actual)
 
 let mutable beforeCallback = false
 let mutable afterCallback = false
@@ -47,12 +25,27 @@ with
     override this.OnAfterDeserialize() =
       afterCallback <- true
 
-[<Fact>]
-let ``receive callback`` () =
-  let input = Call
-  let actual = convert input
-  Assert.True(beforeCallback)
-  Assert.True(afterCallback)
+open Expecto
+
+let simpleUnionTests =
+  testList "Simple Unions" [
+    test "simple" {
+      roundTrip A
+      roundTrip <| B 100
+      roundTrip <| C(99999999L, -123.43f)
+    }
+
+    test "string key" {
+      roundTrip <| D 1
+    }
+
+    test "receive callback" {
+      roundTrip Call
+
+      Expect.isTrue beforeCallback "before callback"
+      Expect.isTrue afterCallback "after callback"
+    }
+  ]
 
 module Compatibility =
 
@@ -92,26 +85,6 @@ module Compatibility =
 
     interface CsSimpleUnion
 
-  [<Fact>]
-  let simple () =
-
-    let input = A
-    let actual = convert<SimpleUnion, CsSimpleUnion> input |> box
-    Assert.True(actual :? CsA)
-
-    let input = B 100
-    match convert<SimpleUnion, CsSimpleUnion> input |> box with
-    | :? CsB as actual ->
-      Assert.Equal(100, actual.Item)
-    | actual -> Assert.True(false, sprintf "expected: CsB, but was: %A" actual)
-
-    let input = C(99999999L, -123.43f)
-    match convert<SimpleUnion, CsSimpleUnion> input  |> box with
-    | :? CsC as actual ->
-      Assert.Equal(99999999L, actual.Item1)
-      Assert.Equal(-123.43f, actual.Item2)
-    | actual -> Assert.True(false, sprintf "expected: CsC, but was: %A" actual)
-
   [<Union(0, typeof<CsD>)>]
   type CsStringKeyUnion = interface end
 
@@ -122,11 +95,56 @@ module Compatibility =
 
     interface CsStringKeyUnion
 
-  [<Fact>]
-  let ``string key`` () =
+  let compatililityTests =
+    testList "Compatibility tests" [
+      test "box and cast conversions" {
+        let input = A
+        let actual = convert<SimpleUnion, CsSimpleUnion> input |> box
+        Expect.isTrue (actual :? CsA) "null case"
 
-    let input = D 100
-    match convert<StringKeyUnion, CsStringKeyUnion> input  |> box with
-    | :? CsD as actual ->
-      Assert.Equal(100, actual.Prop)
-    | actual -> Assert.True(false, sprintf "expected: CsD, but was: %A" actual)
+        let input = B 100
+        match convert<SimpleUnion, CsSimpleUnion> input |> box with
+        | :? CsB as actual ->
+          Expect.equal 100 actual.Item "int case"
+        | actual ->
+          Expect.isTrue false (sprintf "expected: CsB, but was: %A" actual)
+
+        let input = C(99999999L, -123.43f)
+        match convert<SimpleUnion, CsSimpleUnion> input  |> box with
+        | :? CsC as actual ->
+          Expect.equal 99999999L actual.Item1 "C item1"
+          Expect.equal -123.43f actual.Item2 "C item2"
+        | actual ->
+          Expect.isTrue false <| sprintf "expected: CsC, but was: %A" actual
+      }
+
+      test "string key" {
+        let input = D 100
+        match convert<StringKeyUnion, CsStringKeyUnion> input  |> box with
+        | :? CsD as actual ->
+          Expect.equal 100 actual.Prop "CsD"
+        | actual ->
+          Expect.isTrue false <| sprintf "expected: CsD, but was: %A" actual
+      }
+    ]
+
+
+type NestedChild =
+  | CaseChildA of int
+
+type NestedParent =
+  | NestedChild of NestedChild
+  | CaseParentA
+
+[<Test>]
+let nested () =
+  let aNestedDU = NestedChild (CaseChildA 0)
+  // let anotherNestedDU = NestedChild (CaseChildA 0)
+  // let actual = anotherNestedDU
+
+  let actual = convert aNestedDU
+
+  Assert.AreEqual(actual, aNestedDU)
+  assert (actual = aNestedDU)
+  printfn "original %A" aNestedDU
+  printfn "actual %A" actual
